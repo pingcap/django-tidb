5e8db6bd9bedb0e620bdf4874ec7d8f990fe4279
Daniël van Eeden <git@myname.nl>
2023-03-17 08:25:47 +0100

    CI: Add Python 3.11

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     install_requires=["mysqlclient"],
     project_urls={
