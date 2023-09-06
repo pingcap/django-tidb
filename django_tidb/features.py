1c3fc113a4d6b5131597dfd7aa2d06793ea2cc0a
wd0517 <me@wangdi.ink>
2023-09-06 17:11:09 +0800

    fix: add unique constraint after add field

--- django_tidb/features.py
+++ django_tidb/features.py
@@ -95,7 +95,7 @@ class DatabaseFeatures(MysqlDatabaseFeatures):
                 "check_framework.test_database.DatabaseCheckTests.test_mysql_strict_mode",
                 # Unsupported add column and foreign key in single statement
                 "indexes.tests.SchemaIndexesMySQLTests.test_no_index_for_foreignkey",
-                # https://github.com/pingcap/django-tidb/issues/46
+                # TiDB does not support `JSON` format for `EXPLAIN ANALYZE`
                 "queries.test_explain.ExplainTests.test_mysql_analyze",
                 "queries.test_explain.ExplainTests.test_mysql_text_to_traditional",
                 # TiDB cannot guarantee to always rollback the main thread txn when deadlock occurs
@@ -125,10 +125,7 @@ class DatabaseFeatures(MysqlDatabaseFeatures):
                 "schema.tests.SchemaTests.test_ci_cs_db_collation",
                 # Unsupported drop integer primary key
                 "schema.tests.SchemaTests.test_primary_key",
-                "schema.tests.SchemaTests.test_indexes",
-                "schema.tests.SchemaTests.test_remove_db_index_doesnt_remove_custom_indexes",
                 "schema.tests.SchemaTests.test_add_auto_field",
-                "schema.tests.SchemaTests.test_add_field_o2o_nullable",
                 "schema.tests.SchemaTests.test_alter_autofield_pk_to_smallautofield_pk",
                 "schema.tests.SchemaTests.test_alter_primary_key_db_collation",
                 "schema.tests.SchemaTests.test_alter_primary_key_the_same_name",
