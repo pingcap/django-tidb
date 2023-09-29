51bef059c1d86374013cb1f97f8820650b228983
WD <me@wangdi.ink>
2023-09-29 18:37:21 +0800

    feat: support AUTO_RANDOM (#55)
    auto_random is a feature in TiDB that generates unique IDs for a table automatically. It is similar to auto_increment in MySQL, but it can avoid the write hotspot issue caused by auto_increment.

--- django_tidb/base.py
+++ django_tidb/base.py
@@ -42,6 +42,11 @@ class DatabaseWrapper(MysqlDatabaseWrapper):
     introspection_class = DatabaseIntrospection
     ops_class = DatabaseOperations
 
+    data_types = {
+        **MysqlDatabaseWrapper.data_types,
+        "BigAutoRandomField": "bigint AUTO_RANDOM(%(shard_bits)s, %(range)s)",
+    }
+
     def get_database_version(self):
         return self.tidb_version
 
